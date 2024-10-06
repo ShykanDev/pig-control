f8a954beb6d4d1426366876796c9dd208484edf3
awscodeapp@gmail.com <awscodeapp@gmail.com>
2024-10-06 00:08:00 -0600

    Changed to WebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory,  RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { UseSystemValues } from '@/store/UseSystemValues'
 
@@ -49,7 +49,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
